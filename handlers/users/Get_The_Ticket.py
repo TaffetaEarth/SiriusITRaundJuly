855c2a1ae6da621c615cc2117257a53c1b28e9a2
TaffetaEarth <yakovyatchenko@yandex.ru>
2021-07-29 15:18:40 +0300

    Релиз

--- handlers/users/Get_The_Ticket.py
+++ handlers/users/Get_The_Ticket.py
@@ -1,12 +1,8 @@
-from statistics import mean
-
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from aiogram.types import ReplyKeyboardRemove, reply_keyboard
 
-from filters.ListFilter import ListFilter
-from keyboards.default.confirm_keyboard import confirm
 from keyboards.default.description_keyboard import description_key
 from keyboards.default.get_qr_keyboard import get_qr
 from keyboards.default.question_keyboard import question
@@ -69,8 +65,6 @@ async def my_orders(message: types.Message, state: FSMContext):
         list_of_orders = []
         order_list = []
         for i in range(len(orders)):
-            # name_of_event = db.select_event(orders[i][0])[0], db.select_event(orders[i][0])[1],\
-            #                 db.select_event(orders[i][0])[2], db.select_event(orders[i][0])[3]
             event = db.select_event(orders[i][0])
             print(event)
             if len(str(int(event[0][1]) % 100)) == 1:
@@ -97,22 +91,6 @@ async def my_orders(message: types.Message, state: FSMContext):
         await state.set_state("my_orders")
 
 
-# @dp.message_handler(state="start")
-# async def get_list(message: types.Message, state: FSMContext):
-#     data = db.get_list_of_events()
-#     print(data)
-#     list_of_events = []
-#     for i in range(len(data)):
-#         event = data[i]
-#         list_of_events.append(str(i + 1) + '.' + event[-1])
-#     single_event = "\n".join(list_of_events)
-#     await message.answer(f"Вот список ближайших мероприятий: \n"
-#                          f"{single_event}")
-#     await message.answer(f"Выберите, пожалуйста, номер интересующего вас мероприятия",
-#                          reply_markup=ReplyKeyboardRemove())
-#     await state.set_state("list_got")
-
-
 @dp.message_handler(text="Описание", state="amount_got")
 async def description(message: types.Message, state: FSMContext):
     data = await state.get_data()
@@ -123,66 +101,6 @@ async def description(message: types.Message, state: FSMContext):
     await message.answer(f"Введите количество мест,"
                          f" которые вы хотите забронировать")
     await state.set_state("amount_got")
-
-
-#
-# @dp.message_handler(state="description_got")
-# async def choose_event(message: types.Message, state: FSMContext):
-#     if message.text.isdigit():
-#         getted = await state.get_data()
-#         event_id = getted.get("event_id")
-#         data = db.select_event(id=event_id)
-#         single_event = []
-#         for i in range(len(data)):
-#             event = data[i]
-#             if len(str(int(event[1]) % 100)) == 1:
-#                 day: str = "0" + str(int(event[1]) % 100)
-#             else:
-#                 day: str = str(int(event[1]) % 100)
-#             if len(str(int(event[1]) // 100)) == 1:
-#                 month: str = "0" + str(int(event[1]) // 100)
-#             else:
-#                 month: str = str(int(event[1]) // 100)
-#             if len(str(int(event[2]) % 100)) == 1:
-#                 minutes: str = "0" + str(int(event[2]) % 100)
-#             else:
-#                 minutes: str = str(int(event[2]) % 100)
-#             single_event.append(event[0] + '\n' + day + "/" + month
-#                                 + " " + str(int(event[2]) // 100) + ":" + minutes + '\n' + event[3])
-#         one_event = "\n".join(single_event)
-#         amount = db.get_amount_of_places(event_id)[0]
-#         handled = db.get_handled_places(id=event_id)
-#         print(handled)
-#         amount_of_handled = 0
-#         for f in range(len(handled)):
-#             amount_of_handled += int(handled[f][0])
-#         await state.update_data(one_event=one_event)
-#         print(amount - amount_of_handled)
-#         if amount - amount_of_handled > 0:
-#             await message.answer(f"Вы выбрали событие: \n"
-#                                  f"{one_event} \n"
-#                                  f"Доступно {int(amount - amount_of_handled)} мест")
-#             await message.answer(f"Введите количество мест,"
-#                                  f" которые вы хотите забронировать")
-#             await state.set_state("amount_got")
-#         elif amount - amount_of_handled == 0:
-#             await message.answer("К сожалению, мест больше нет \n"
-#                                  "Пожалуйста, выберите другое мероприятие")
-#             data = db.get_list_of_events()
-#             list_of_events = []
-#             for i in range(len(data)):
-#                 event = data[i]
-#                 list_of_events.append(event[0] + '.' + event[-1])
-#             single_event = "\n".join(list_of_events)
-#             await message.answer(f"Вот список ближайших мероприятий: \n"
-#                                  f"{single_event}")
-#             await message.answer(f"Выберите, пожалуйста, номер интересующего вас мероприятия")
-#             await state.set_state("list_got")
-#         else:
-#             await message.answer(f"К сожалению, свободных мест не хватает. \n"
-#                                  f"Пожалуйста, выберите корректное количество мест")
-#     else:
-#         await message.answer("Вы ввели неправильный формат данных, пожалуйста, введите номер с помощью цифр")
 
 
 @dp.message_handler(state="list_got")
@@ -312,8 +230,6 @@ async def my_orders(message: types.Message, state: FSMContext):
         list_of_orders = []
         order_list = []
         for i in range(len(orders)):
-            # name_of_event = db.select_event(orders[i][0])[0], db.select_event(orders[i][0])[1],\
-            #                 db.select_event(orders[i][0])[2], db.select_event(orders[i][0])[3]
             event = db.select_event(orders[i][0])
             print(event)
             if len(str(int(event[0][1]) % 100)) == 1:
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.types import ReplyKeyboardRemove
 
 from loader import dp
 
@@ -10,4 +11,5 @@ async def bot_start(message: types.Message, state: FSMContext):
     await message.answer(f"Привет, {message.from_user.full_name}! \n"
                          f"Я - СириусБот! \n"
                          f"Я расскажу тебе о том, что происходит на Федеральной территории Сириус. \n"
-                         f"Нажми /get_list, чтобы увидеть, какие события ожидают тебя здесь в ближайшее время.")
+                         f"Нажми /get_list, чтобы увидеть, какие события ожидают тебя здесь в ближайшее время.",
+                         reply_markup=ReplyKeyboardRemove())
